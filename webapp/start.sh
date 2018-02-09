77404b22edd6eee47a062aefa8f60215bec4d8de
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-09 15:05:04 -0800

    Be sure to set working directory

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,1 +1,5 @@
+# cd to location of this file
+cd "$(dirname "$(test -L "${BASH_SOURCE[0]}" && readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")"
+
+# start server with forever
 NODE_ENV=production DEBUG=app:* forever -a -o log/out.log -e log/err.log start server.js
