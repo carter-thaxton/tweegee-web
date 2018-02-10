c480776a356ae8515b9f347efda6b23c6c1a830f
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-09 16:34:34 -0800

    Fix start.sh

--- webapp/start.sh
+++ webapp/start.sh
@@ -2,7 +2,7 @@
 cd "$(dirname "$(test -L "${BASH_SOURCE[0]}" && readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")"
 
 # Make sure node is loaded
-[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.rvm/nvm.sh"
+[[ -s "$HOME/.nvm/nvm.sh" ]] && \. "$HOME/.nvm/nvm.sh"
 
 # start server with forever
 NODE_ENV=production DEBUG=app:* forever -a -o log/out.log -e log/err.log start server.js
