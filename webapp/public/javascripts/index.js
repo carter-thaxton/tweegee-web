5b533550001425a0586562688d2131e474e4d428
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-09 18:45:44 -0800

    Show some response to upload and minor error handlind

--- webapp/public/javascripts/index.js
+++ webapp/public/javascripts/index.js
@@ -1,23 +1,38 @@
 $(function() {
-  console.log("Loaded!")
 
+  // Setup form upload
   $('form').submit(function(evt) {
     evt.preventDefault();
-    var formData = new FormData($(this)[0]);
-    $.ajax({
-      url: 'file_upload',
-      type: 'POST',
-      data: formData,
-      async: true,
-      cache: false,
-      contentType: false,
-      enctype: 'multipart/form-data',
-      processData: false,
-      success: function(response) {
-        console.log(response);
-      }
-    });
+    var file = $('#file').val().trim();
+    if (file) {
+      var formData = new FormData($(this)[0]);
+      console.log("Sending file...");
+      $.ajax({
+        url: 'file_upload',
+        type: 'POST',
+        data: formData,
+        async: true,
+        cache: false,
+        contentType: false,
+        enctype: 'multipart/form-data',
+        processData: false,
+        success: show_result,
+        error: handle_error,
+      });
+    } else {
+      console.log("No file selected");
+    }
     return false;
   });
+
+  function handle_error(error) {
+    alert(JSON.stringify(error, null, 2));
+  }
+
+  function show_result(response) {
+    console.log(response);
+    var result_text = JSON.stringify(response, null, 2);
+    $('.results').text(result_text);
+  }
 
 });
