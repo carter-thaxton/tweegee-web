2363467d45a8c870fdfc5bfde242d22fd741bf6c
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-09 19:41:38 -0800

    Make sure rvm is loaded before running webapp.  It uses ruby in html2twee

--- webapp/start.sh
+++ webapp/start.sh
@@ -6,5 +6,8 @@ cd "$(dirname "$(test -L "${BASH_SOURCE[0]}" && readlink "${BASH_SOURCE[0]}" || 
 # Make sure node is loaded
 [[ -s "$HOME/.nvm/nvm.sh" ]] && \. "$HOME/.nvm/nvm.sh"
 
+# Make sure ruby is loaded with rvm
+[[ -s /etc/profile.d/rvm.sh ]] && \. /etc/profile.d/rvm.sh
+
 # start server with forever
 NODE_ENV=production DEBUG=app:* forever -a -o log/out.log -e log/err.log start server.js
