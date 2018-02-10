76c95666e4b3f04bebd0b88bd694a618953bc0f4
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-09 16:31:24 -0800

    Make sure node is loaded in start.sh

--- webapp/start.sh
+++ webapp/start.sh
@@ -1,5 +1,8 @@
 # cd to location of this file
 cd "$(dirname "$(test -L "${BASH_SOURCE[0]}" && readlink "${BASH_SOURCE[0]}" || echo "${BASH_SOURCE[0]}")")"
 
+# Make sure node is loaded
+[[ -s "$HOME/.nvm/nvm.sh" ]] && . "$HOME/.rvm/nvm.sh"
+
 # start server with forever
 NODE_ENV=production DEBUG=app:* forever -a -o log/out.log -e log/err.log start server.js
