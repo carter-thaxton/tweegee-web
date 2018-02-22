ac874fa2a36144a22e507037b8186e881a571617
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-21 16:25:34 -0800

    UI improvements to file upload

--- webapp/public/javascripts/index.js
+++ webapp/public/javascripts/index.js
@@ -1,12 +1,30 @@
 $(function() {
+  var uploading = false
+
+  function update_upload_visibility() {
+    var disabled
+    if (uploading) {
+      disabled = true
+    } else {
+      var file = $('#file').val().trim()
+      disabled = !file
+    }
+    $('#submit').prop('disabled', disabled)
+  }
+
+  $('#file').change(update_upload_visibility)
+  update_upload_visibility()
 
   // Setup form upload
-  $('form').submit(function(evt) {
+  $('#upload_form').submit(function(evt) {
     evt.preventDefault()
     var file = $('#file').val().trim()
     if (file) {
       var formData = new FormData($(this)[0])
       console.log("Sending file...")
+      uploading = true
+      update_upload_visibility()
+
       $.ajax({
         url: 'file_upload',
         type: 'POST',
@@ -19,19 +37,21 @@ $(function() {
         success: show_result,
         error: handle_error,
       })
-    } else {
-      console.log("No file selected")
     }
     return false
   })
 
   function handle_error(error) {
+    uploading = false
+    update_upload_visibility()
     var errText = JSON.stringify(error, null, 2)
     console.log(errText)
     alert(errText)
   }
 
   function show_result(response) {
+    uploading = false
+    update_upload_visibility()
     console.log(response)
     var stdout = response.stdout
     delete response.stdout
