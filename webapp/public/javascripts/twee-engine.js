ddd926f7cf9c8d5399cc4ab67265eeca50886e8b
Carter Thaxton <carter.thaxton@gmail.com>
2018-02-27 16:41:16 -0800

    Fix quoted expressions, and refactor twee engine

--- webapp/public/javascripts/twee-engine.js
+++ webapp/public/javascripts/twee-engine.js
@@ -60,75 +60,24 @@ class TweeEngine {
       throw new Error('Awaiting a choice.  Must make choice with gotoPassage')
     }
 
-    var result = null
     const stmt = this.nextStatement()
-
     if (stmt) {
-      // advance index within block.  some statements like 'if' or 'link' will alter this further.
+      // advance index within block
+      // some statements like 'if' or 'link' may alter this further by pushing a block or going to another passage
       this.statementIndex += 1
-
-      switch (stmt._type) {
-        case 'newline':
-        const message = this.currentLine.trim()
-        if (message) {
-          result = { type: 'message', text: message }
-        }
-        this.currentLine = ""
-        break
-
-        case 'text':
-        this.currentLine += stmt.text
-        break
-
-        case 'set':
-        this.variables[stmt.variable] = this.interpretExpression(stmt.expression)
-        break
-
-        case 'link':
-        if (this.currentLine) throw new Error('Did not emit newline before link')
-        this.gotoPassage(stmt.name)
-        break
-
-        case 'delay':
-        const delayExpr = stmt.expression
-        this.pushBlock(stmt.statements, () => {
-          const text = this.currentLine
-          this.currentLine = ""
-          return { type: 'delay', delay: delayExpr, text: text }
-        })
-        break
-
-        case 'choice':
-        result = { type: 'choice', choices: stmt.choices }
-        this.awaitingChoice = true
-        break
-
-        case 'if':
-        for (const clause of stmt.clauses) {
-          // no condition means else clause
-          const ok = !clause.condition || this.interpretExpression(clause.condition)
-          if (ok) {
-            this.pushBlock(clause.statements)
-            break
-          }
-        }
-        break
-
-        default:
-        throw new Error('Unknown statement type: ' + stmt._type)
-      }
+      return this.interpretStatement(stmt)
     } else if (this.isNestedBlock()) {
       // finished previous block.  pop nested statement.
       // if popped statement has a callback, call it and it may also produce a result.
       const popFn = this.popBlock()
       if (popFn) {
-        result = popFn()
+        return popFn()
+      } else {
+        return null
       }
     } else {
       throw new Error('No next statement')
     }
-
-    return result
   }
 
 
@@ -150,6 +99,83 @@ class TweeEngine {
       return this.nestedCallbacks.pop()
   }
 
+  interpretStatement(stmt) {
+    // Print out for now
+    console.log(stmt)
+
+    var result = null
+    switch (stmt._type) {
+      case 'newline':
+      const message = this.currentLine.trim()
+      if (message) {
+        result = { type: 'message', text: message }
+      }
+      this.currentLine = ""
+      break
+
+      case 'text':
+      this.currentLine += stmt.text
+      break
+
+      case 'expression':
+      this.currentLine += this.interpretExpression(stmt.expression)
+      break
+
+      case 'set':
+      this.variables[stmt.variable] = this.interpretExpression(stmt.expression)
+      break
+
+      case 'link':
+      if (this.currentLine) throw new Error('Did not emit newline before link')
+      this.gotoPassage(stmt.name)
+      break
+
+      case 'delay':
+      const delayExpr = stmt.expression
+      this.pushBlock(stmt.statements, () => {
+        const text = this.currentLine
+        this.currentLine = ""
+        return { type: 'delay', delay: delayExpr, text: text }
+      })
+      break
+
+      case 'choice':
+      result = { type: 'choice', choices: stmt.choices }
+      this.awaitingChoice = true
+      break
+
+      case 'include':
+      const includePassageName = this.interpretExpression(stmt.passageExpr)
+      const includePassage = this.passagesByName[includePassageName]
+      const returnToPassage = this.currentPassageName
+      if (includePassage) {
+        this.currentPassageName = includePassageName
+        this.pushBlock(includePassage.statements, () => {
+          this.currentPassageName = returnToPassage
+        })
+      } else {
+        throw new Error('Could not include passage named: ' + includePassageName)
+      }
+      break
+
+      case 'if':
+      for (const clause of stmt.clauses) {
+        // no condition means else clause
+        const ok = !clause.condition || this.interpretExpression(clause.condition)
+        if (ok) {
+          this.pushBlock(clause.statements)
+          break
+        }
+      }
+      break
+
+      default:
+      throw new Error('Unknown statement type: ' + stmt._type)
+    }
+
+    return result
+  }
+
   interpretExpression(expr) {
     const keywords = ['true', 'false', 'null']
 
@@ -157,9 +183,14 @@ class TweeEngine {
       .replace(/\bis\b/g, '==')
       .replace(/\bor\b/g, '||')
       .replace(/\band\b/g, '&&')
-      .replace(/[$_a-zA-Z]\w*/g, (v) => {
-        if (keywords.indexOf(v) >= 0) return v
-        else return this.variables[v]
+      .replace(/("[^"]*"|'[^']*')|([$_a-zA-Z]\w*)/g, (match, quoted, variable) => {
+        if (quoted) { return quoted }
+        else if (variable) {
+          if (keywords.indexOf(variable) >= 0) return variable
+          return this.variables[variable]
+        } else {
+          throw new Error('Error while replacing variables in expression: ' + expr)
+        }
       })
 
     try {
